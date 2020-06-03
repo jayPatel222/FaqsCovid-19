ece42a8c74017db0befc101cf562b1ab2265035b
Jay Patel <44516786+jayPatel222@users.noreply.github.com>
2020-06-02 21:25:45 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/faqscovid_19/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.faqscovid_19;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle( this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new newsFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_news);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_news:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new newsFragment()).commit();
+                break;
+            case R.id.nav_data:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new dataFragment()).commit();
+                break;
+            case R.id.nav_symptoms:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new symptomsFragment()).commit();
+                break;
+            case R.id.nav_prevention:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new preventionFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/faqscovid_19/symptomsFragment.java
@@ -0,0 +1,18 @@
+package com.example.faqscovid_19;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class symptomsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_symptoms, container, false);
+    }
+}
+
